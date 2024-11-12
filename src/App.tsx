280911174929512ac59112379f155e4ac2963df3
xoel1991 <153995406+xoel1991@users.noreply.github.com>
2024-11-12 13:52:21 +0900

    add map id

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
-// import { NavermapsProvider } from 'react-naver-maps';
+import { NavermapsProvider } from 'react-naver-maps';
 import { Heading1 } from '@/components/Text.tsx';
 import Wrapper from '@/components/Wrapper.tsx';
 import Account from '@/layout/Account/Account.tsx';
@@ -8,11 +8,13 @@ import FloatingBar from '@/layout/FloatingBar/FloatingBar.tsx';
 import GalleryWrap from '@/layout/Gallery/GalleryWrap.tsx';
 // import Guestbook from '@/layout/Guestbook/Guestbook.tsx';
 import Invitation from '@/layout/Invitation/Invitation.tsx';
-// import Location from '@/layout/Location/Location.tsx';
+import Location from '@/layout/Location/Location.tsx';
 import Main from '@/layout/Main/Main.tsx';
 
 function App() {
   // const ncpClientId = import.meta.env.VITE_APP_NAVERMAPS_CLIENT_ID;
+  // const ncpClientId: string = process.env.REACT_APP_API_KEY ? process.env.REACT_APP_API_KEY : '';
+  const ncpClientId = 'xlkmt8gilu';
   const [isVisible, setIsVisible] = useState(false);
   const galleryRef = useRef(null);
 
@@ -37,34 +39,34 @@ function App() {
   };
 
   return (
-    // <NavermapsProvider ncpClientId={ncpClientId}>
-    <Container>
-      <Wrapper>
-        <Main />
-      </Wrapper>
-      <Wrapper>
-        <Heading1>모시는 글</Heading1>
-        <Invitation />
-      </Wrapper>
-      <Wrapper ref={galleryRef}>
-        <Heading1>Gallery</Heading1>
-        <GalleryWrap />
-      </Wrapper>
-      <Wrapper>
-        <Heading1>마음 전하실 곳</Heading1>
-        <Account />
-      </Wrapper>
-      <Wrapper>
-        <Heading1>오시는 길</Heading1>
-        {/* <Location /> */}
-      </Wrapper>
-      <Wrapper>
-        {/* <Heading1>신랑 신부에게</Heading1> */}
-        {/* <Guestbook /> */}
-      </Wrapper>
-      <FloatingBar isVisible={isVisible} />
-    </Container>
-    // </NavermapsProvider>
+    <NavermapsProvider ncpClientId={ncpClientId}>
+      <Container>
+        <Wrapper>
+          <Main />
+        </Wrapper>
+        <Wrapper>
+          <Heading1>모시는 글</Heading1>
+          <Invitation />
+        </Wrapper>
+        <Wrapper ref={galleryRef}>
+          <Heading1>Gallery</Heading1>
+          <GalleryWrap />
+        </Wrapper>
+        <Wrapper>
+          <Heading1>마음 전하실 곳</Heading1>
+          <Account />
+        </Wrapper>
+        <Wrapper>
+          <Heading1>오시는 길</Heading1>
+          <Location />
+        </Wrapper>
+        <Wrapper>
+          {/* <Heading1>신랑 신부에게</Heading1> */}
+          {/* <Guestbook /> */}
+        </Wrapper>
+        <FloatingBar isVisible={isVisible} />
+      </Container>
+    </NavermapsProvider>
   );
 }
 
--- src/layout/Location/Map.tsx
+++ src/layout/Location/Map.tsx
@@ -14,9 +14,9 @@ const Map = () => {
       <NaverMap
         defaultCenter={new navermaps.LatLng(lat, lon)}
         defaultZoom={17}
-        draggable={false}
-        pinchZoom={false}
-        scrollWheel={false}
+        draggable={true}
+        pinchZoom={true}
+        scrollWheel={true}
         keyboardShortcuts={false}>
         <Marker defaultPosition={new navermaps.LatLng(lat, lon)} />
       </NaverMap>
