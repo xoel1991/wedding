0c7a68a1d7938adbb8596d53845fee3e83baa44a
xoel1991 <153995406+xoel1991@users.noreply.github.com>
2024-12-04 17:24:22 +0900

    change port

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgr()],
   base: '/wedding/',
+  server: {
+    port: 3000, // 원하는 포트로 변경
+  },
 });
