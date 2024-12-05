394c4b4c6ce389f06338bcd8ca106c69c9deb1d4
xoel1991 <153995406+xoel1991@users.noreply.github.com>
2024-12-05 11:04:28 +0900

    hide map

--- src/layout/Location/Location.tsx
+++ src/layout/Location/Location.tsx
@@ -1,8 +1,8 @@
 import styled from '@emotion/styled';
 import data from 'data.json';
 import Address from './Address.tsx';
-import Map from './Map.tsx';
-import MapButtons from './MapButtons.tsx';
+// import Map from './Map.tsx';
+// import MapButtons from './MapButtons.tsx';
 import { Caption, PointTitle } from '@/components/Text.tsx';
 
 const Location = () => {
@@ -11,8 +11,8 @@ const Location = () => {
     <LocationWrapper>
       <PointTitle>{mapInfo.address1}</PointTitle>
       <Caption textAlign={'center'}>{mapInfo.address2}</Caption>
-      <Map />
-      <MapButtons />
+      {/* <Map />
+      <MapButtons /> */}
       <Address />
     </LocationWrapper>
   );
