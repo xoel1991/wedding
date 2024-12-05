fcdc5aefac4071f5c88645dc7e4c12971802204e
xoel1991 <153995406+xoel1991@users.noreply.github.com>
2024-12-05 11:10:08 +0900

    change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgr()],
-  base: '/wedding/',
+  // base: '/wedding/',
   server: {
     port: 3000, // 원하는 포트로 변경
   },
