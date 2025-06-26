afdbddac60ac7254e98e0133c90190f77de260af
xoel1991 <153995406+xoel1991@users.noreply.github.com>
2025-06-26 13:35:44 +0900

    change kakao sdk type location name test

--- src/layout/FloatingBar/FloatingBar.tsx
+++ src/layout/FloatingBar/FloatingBar.tsx
@@ -82,7 +82,7 @@ const FloatingBar = ({ isVisible }: { isVisible: boolean }) => {
     // });
     window.Kakao.Share.sendDefault({
       objectType: 'location',
-      address: '서울 송파구 송파대로 155 송파농협신청사 11-13층',
+      address: '더컨벤션 송파문정점 ',
       addressTitle: '더컨벤션 송파문정 13층 아모르홀',
       content: {
         title: '강태희 🖤 김지원 결혼합니다!',
