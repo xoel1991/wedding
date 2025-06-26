a39984e0bfce33fc76f75c67cb831f245e052562
xoel1991 <153995406+xoel1991@users.noreply.github.com>
2025-06-26 15:23:10 +0900

    change gallery box color

--- src/layout/Gallery/GalleryWrap.tsx
+++ src/layout/Gallery/GalleryWrap.tsx
@@ -55,7 +55,7 @@ const PlusButton = styled.div`
   padding: 6px 12px;
   font-size: 1rem;
   align-items: center;
-  border-radius: 4px;
+  border-radius: 8px;
   border: 1px solid #e88ca6;
   cursor: pointer;
 `;
