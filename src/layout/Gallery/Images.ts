3c701c6c43dfb581c8cdf14887b3d1b775135e92
xoel1991 <153995406+xoel1991@users.noreply.github.com>
2025-08-01 12:25:31 +0900

    change gallery file

--- src/layout/Gallery/Images.ts
+++ src/layout/Gallery/Images.ts
@@ -1,36 +1,61 @@
-import image01 from '@/assets/images/01.jpg';
-import image02 from '@/assets/images/02.jpg';
-import image03 from '@/assets/images/03.jpg';
-import image04 from '@/assets/images/04.jpg';
-import image05 from '@/assets/images/05.jpg';
-import image06 from '@/assets/images/06.jpg';
-import image07 from '@/assets/images/07.jpg';
-import image08 from '@/assets/images/08.jpg';
-import image09 from '@/assets/images/09.jpg';
-import image10 from '@/assets/images/10.jpg';
-import image11 from '@/assets/images/11.jpg';
-import image12 from '@/assets/images/12.jpg';
-import image13 from '@/assets/images/13.jpg';
-import image14 from '@/assets/images/14.jpg';
-import image15 from '@/assets/images/15.jpg';
-import image16 from '@/assets/images/16.jpg';
-import image17 from '@/assets/images/17.jpg';
-import image18 from '@/assets/images/18.jpg';
-import image19 from '@/assets/images/19.jpg';
-import image20 from '@/assets/images/20.jpg';
-import image21 from '@/assets/images/21.jpg';
-import image22 from '@/assets/images/22.jpg';
-import image23 from '@/assets/images/23.jpg';
-import image24 from '@/assets/images/24.jpg';
-import image25 from '@/assets/images/25.jpg';
-import image26 from '@/assets/images/26.jpg';
-import image27 from '@/assets/images/27.jpg';
-import image28 from '@/assets/images/28.jpg';
-import image29 from '@/assets/images/29.jpg';
-import image30 from '@/assets/images/30.jpg';
-import image31 from '@/assets/images/31.jpg';
-import image32 from '@/assets/images/32.jpg';
-import image33 from '@/assets/images/33.jpg';
+import image01 from '@/assets/images/01.jpeg';
+import image02 from '@/assets/images/02.jpeg';
+import image03 from '@/assets/images/03.jpeg';
+import image04 from '@/assets/images/04.jpeg';
+import image05 from '@/assets/images/05.jpeg';
+import image06 from '@/assets/images/06.jpeg';
+import image07 from '@/assets/images/07.jpeg';
+import image08 from '@/assets/images/08.jpeg';
+import image09 from '@/assets/images/09.jpeg';
+import image10 from '@/assets/images/10.jpeg';
+import image11 from '@/assets/images/11.jpeg';
+import image12 from '@/assets/images/12.jpeg';
+import image13 from '@/assets/images/13.jpeg';
+import image14 from '@/assets/images/14.jpeg';
+import image15 from '@/assets/images/15.jpeg';
+import image16 from '@/assets/images/16.jpeg';
+import image17 from '@/assets/images/17.jpeg';
+import image18 from '@/assets/images/18.jpeg';
+import image19 from '@/assets/images/19.jpeg';
+import image20 from '@/assets/images/20.jpeg';
+import image21 from '@/assets/images/21.jpeg';
+import image22 from '@/assets/images/22.jpeg';
+import image23 from '@/assets/images/23.jpeg';
+import image24 from '@/assets/images/24.jpeg';
+import image25 from '@/assets/images/25.jpeg';
+import image26 from '@/assets/images/26.jpeg';
+import image27 from '@/assets/images/27.jpeg';
+import image28 from '@/assets/images/28.jpeg';
+import image29 from '@/assets/images/29.jpeg';
+import image30 from '@/assets/images/30.jpeg';
+import image31 from '@/assets/images/31.jpeg';
+import image32 from '@/assets/images/32.jpeg';
+import image33 from '@/assets/images/33.jpeg';
+import image34 from '@/assets/images/34.jpeg';
+import image35 from '@/assets/images/35.jpeg';
+import image36 from '@/assets/images/36.jpeg';
+import image37 from '@/assets/images/37.jpeg';
+import image38 from '@/assets/images/38.jpeg';
+import image39 from '@/assets/images/39.jpeg';
+import image40 from '@/assets/images/40.jpeg';
+import image41 from '@/assets/images/41.jpeg';
+import image42 from '@/assets/images/42.jpeg';
+import image43 from '@/assets/images/43.jpeg';
+import image44 from '@/assets/images/44.jpeg';
+import image45 from '@/assets/images/45.jpeg';
+import image46 from '@/assets/images/46.jpeg';
+import image47 from '@/assets/images/47.jpeg';
+import image48 from '@/assets/images/48.jpeg';
+import image49 from '@/assets/images/49.jpeg';
+import image50 from '@/assets/images/50.jpeg';
+import image51 from '@/assets/images/51.jpeg';
+import image52 from '@/assets/images/52.jpeg';
+import image53 from '@/assets/images/53.jpeg';
+import image54 from '@/assets/images/54.jpeg';
+import image55 from '@/assets/images/55.jpeg';
+import image56 from '@/assets/images/56.jpeg';
+import image57 from '@/assets/images/57.jpeg';
+
 
 const images = [
   /* 이미지 경로를 리스트로 저장 */
@@ -79,10 +104,8 @@ const images = [
   {
     alt: 'image08',
     source: image08,
-    // width: 640,
-    width: 960,
-    // height: 960,
-    height: 640,
+    width: 640,
+    height: 960,
   },
   {
     alt: 'image09',
@@ -123,18 +146,14 @@ const images = [
   {
     alt: 'image15',
     source: image15,
-    // width: 640,
-    width: 960,
-    // height: 960,
-    height: 640,
+    width: 640,
+    height: 960,
   },
   {
     alt: 'image16',
     source: image16,
-    // width: 640,
-    width: 960,
-    // height: 960,
-    height: 640,
+    width: 640,
+    height: 960,
   },
   {
     alt: 'image17',
@@ -181,10 +200,8 @@ const images = [
   {
     alt: 'image24',
     source: image24,
-    // width: 640,
-    width: 960,
-    // height: 960,
-    height: 640,
+    width: 640,
+    height: 960,
   },
   {
     alt: 'image25',
@@ -225,8 +242,10 @@ const images = [
   {
     alt: 'image31',
     source: image31,
-    width: 640,
-    height: 960,
+    // width: 640,
+    // height: 960,
+    width: 960,
+    height: 640,
   },
   {
     alt: 'image32',
@@ -237,6 +256,155 @@ const images = [
   {
     alt: 'image33',
     source: image33,
+    width: 640,
+    height: 960,
+  },
+  {
+    alt: 'image34',
+    source: image34,
+    width: 640,
+    height: 960,
+  },
+  {
+    alt: 'image35',
+    source: image35,
+    width: 640,
+    height: 960,
+  },
+  {
+    alt: 'image36',
+    source: image36,
+    width: 640,
+    height: 960,
+  },
+  {
+    alt: 'image37',
+    source: image37,
+    // width: 640,
+    // height: 960,
+    width: 960,
+    height: 640,
+  },
+  {
+    alt: 'image38',
+    source: image38,
+    width: 640,
+    height: 960,
+  },
+  {
+    alt: 'image39',
+    source: image39,
+    width: 640,
+    height: 960,
+  },
+  {
+    alt: 'image40',
+    source: image40,
+    width: 640,
+    height: 960,
+  },
+  {
+    alt: 'image41',
+    source: image41,
+    // width: 640,
+    // height: 960,
+    width: 960,
+    height: 640,
+  },
+  {
+    alt: 'image42',
+    source: image42,
+    width: 640,
+    height: 960,
+  },
+  {
+    alt: 'image43',
+    source: image43,
+    width: 640,
+    height: 960,
+  },
+  {
+    alt: 'image44',
+    source: image44,
+    width: 640,
+    height: 960,
+  },
+  {
+    alt: 'image45',
+    source: image45,
+    width: 640,
+    height: 960,
+  },
+  {
+    alt: 'image46',
+    source: image46,
+    width: 640,
+    height: 960,
+  },
+  {
+    alt: 'image47',
+    source: image47,
+    width: 640,
+    height: 960,
+  },
+  {
+    alt: 'image48',
+    source: image48,
+    width: 640,
+    height: 960,
+  },
+  {
+    alt: 'image49',
+    source: image49,
+    width: 640,
+    height: 960,
+  },
+
+  {
+    alt: 'image50',
+    source: image50,
+    width: 640,
+    height: 960,
+  },
+  {
+    alt: 'image51',
+    source: image51,
+    width: 640,
+    height: 960,
+  },
+  {
+    alt: 'image52',
+    source: image52,
+    width: 640,
+    height: 960,
+  },
+  {
+    alt: 'image53',
+    source: image53,
+    width: 640,
+    height: 960,
+  },
+  {
+    alt: 'image54',
+    source: image54,
+    width: 640,
+    height: 960,
+  },
+  {
+    alt: 'image55',
+    source: image55,
+    width: 640,
+    height: 960,
+  },
+  {
+    alt: 'image56',
+    source: image56,
+    width: 640,
+    height: 960,
+  },
+  {
+    alt: 'image57',
+    source: image57,
     width: 640,
     height: 960,
   },
